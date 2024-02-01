3361f1520dfa7929e68dc64c5e49f403dc97923b
I. Kenan Balci <i.kenanbalci@gmail.com>
2024-02-02 01:56:15 +0300

    Game logic and main class added.

---
+++ TicTacToe.cs
@@ -0,0 +1,72 @@
+using System;
+
+class TicTacToe
+{
+    static void Main()
+    {
+        TicTacToeBoard board = new TicTacToeBoard();
+        bool gameOver = false;
+
+        do
+        {
+            Console.Clear();
+            board.PrintBoard();
+
+            if (board.CheckForWin())
+            {
+                Console.WriteLine("Congratulations! You won!");
+                break;
+            }
+
+            if (board.IsBoardFull())
+            {
+                Console.WriteLine("It's a draw!");
+                break;
+            }
+
+            Console.WriteLine("Enter your move (1-9):");
+            int playerMove = int.Parse(Console.ReadLine());
+
+            if (board.IsMoveValid(playerMove))
+            {
+                board.MakeMove(playerMove, 'X');
+
+                if (board.CheckForWin())
+                {
+                    Console.Clear();
+                    board.PrintBoard();
+                    Console.WriteLine("Congratulations! You won!");
+                    gameOver = true;
+                    break;
+                }
+
+                if (board.IsBoardFull())
+                {
+                    Console.Clear();
+                    board.PrintBoard();
+                    Console.WriteLine("It's a draw!");
+                    gameOver = true;
+                    break;
+                }
+
+                Console.Clear();
+                board.PrintBoard();
+                Console.WriteLine("Bot is making a move...");
+
+                int botMove = board.GetBestMove();
+                board.MakeMove(botMove, 'O');
+
+                Console.Clear();
+                board.PrintBoard();
+            }
+            else
+            {
+                Console.WriteLine("Invalid move. Try again.");
+            }
+
+        } while (!gameOver);
+
+        Console.WriteLine("Game Over. Press any key to exit.");
+        Console.ReadKey();
+    }
+}
--- TicTacToeBoard.cs
+++ TicTacToeBoard.cs
@@ -1,8 +1,8 @@
-class TictacToeBoard
+class TicTacToeBoard
 {
     private char[,] board;
 
-    public TictacToeBoard()
+    public TicTacToeBoard()
     {
         board = new char[3, 3];
         InitializeBoard();
@@ -40,5 +40,137 @@ class TictacToeBoard
             if (i < 2) Console.WriteLine("-----------");
         }
         Console.WriteLine();
-    }   
+    }
+
+    public bool CheckForWin()
+    {
+        return CheckRow() || CheckColumn() || CheckDiagonal();
+    }
+
+    private bool CheckRow()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if (board[i, 0] != ' ' && board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+                return true;
+        }
+        return false;
+    }
+
+    private bool CheckColumn()
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (board[0, j] != ' ' && board[0, j] == board[1, j] && board[1, j] == board[2, j])
+                return true;
+        }
+        return false;
+    }
+
+    private bool CheckDiagonal()
+    {
+        return (board[0, 0] != ' ' && board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2]) ||
+               (board[0, 2] != ' ' && board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0]);
+    }
+
+    public bool IsBoardFull()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (board[i, j] == ' ')
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public bool IsMoveValid(int move)
+    {
+        int row = (move - 1) / 3;
+        int col = (move - 1) % 3;
+
+        return move >= 1 && move <= 9 && board[row, col] == ' ';
+    }
+
+    public void MakeMove(int move, char player)
+    {
+        int row = (move - 1) / 3;
+        int col = (move - 1) % 3;
+
+        board[row, col] = player;
+    }
+
+    public void UndoMove(int move)
+    {
+        int row = (move - 1) / 3;
+        int col = (move - 1) % 3;
+
+        board[row, col] = ' ';
+    }
+
+    public int GetBestMove()
+    {
+        int bestMove = -1;
+        int bestScore = int.MinValue;
+
+        for (int i = 1; i <= 9; i++)
+        {
+            if (IsMoveValid(i))
+            {
+                MakeMove(i, 'O');
+                int score = Minimax(0, false);
+                UndoMove(i);
+
+                if (score > bestScore)
+                {
+                    bestScore = score;
+                    bestMove = i;
+                }
+            }
+        }
+
+        return bestMove;
+    }
+
+    private int Minimax(int depth, bool isMaximizingPlayer)
+    {
+        if (CheckForWin())
+            return isMaximizingPlayer ? -1 : 1;
+
+        if (IsBoardFull())
+            return 0;
+
+        if (isMaximizingPlayer)
+        {
+            int maxEval = int.MinValue;
+            for (int i = 1; i <= 9; i++)
+            {
+                if (IsMoveValid(i))
+                {
+                    MakeMove(i, 'O');
+                    int eval = Minimax(depth + 1, false);
+                    UndoMove(i);
+                    maxEval = Math.Max(maxEval, eval);
+                }
+            }
+            return maxEval;
+        }
+        else
+        {
+            int minEval = int.MaxValue;
+            for (int i = 1; i <= 9; i++)
+            {
+                if (IsMoveValid(i))
+                {
+                    MakeMove(i, 'X');
+                    int eval = Minimax(depth + 1, true);
+                    UndoMove(i);
+                    minEval = Math.Min(minEval, eval);
+                }
+            }
+            return minEval;
+        }
+    }
 }
